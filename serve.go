143d32a418d82e882ce087cef670f3a5230a1a39
eyedeekay <hankhill19580@gmail.com>
2025-03-17 18:10:40 -0400

    updata metadata function

--- serve.go
+++ serve.go
@@ -1,6 +1,10 @@
 package jumpserver
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+	"time"
+)
 
 func (j *JumpServer) Serve() error {
 	l, err := j.Garlic.Listen()
@@ -9,4 +13,50 @@ func (j *JumpServer) Serve() error {
 	}
 	defer l.Close()
 	return http.Serve(l, j)
+}
+
+func (j *JumpServer) Close() error {
+	return j.Garlic.Close()
+}
+
+func (j *JumpServer) updateMetadata() error {
+	// Update the metadata of the jump server
+	for _, host := range j.Hostnames {
+		metadata, err := j.Extractor.Extract(host.I2PAddr.Base32())
+		if err != nil {
+			return err
+		}
+		for _, tag := range metadata {
+			if host.Registrant.Description != "" {
+				continue
+			}
+			if host.Time == (time.Time{}) {
+				host.Time = time.Now()
+			}
+			if tag.Name == "title" {
+				host.Registrant.Description = tag.Content
+			}
+			if tag.Name == "author" {
+				host.Registrant.Description = tag.Content
+			}
+			if tag.Name == "description" {
+				host.Registrant.Description = tag.Content
+			}
+			if tag.Name == "keywords" {
+				for _, keyword := range splitTags(tag.Content) {
+					host.Registrant.Tags = append(host.Registrant.Tags, keyword)
+				}
+			}
+
+		}
+	}
+	return nil
+}
+
+func splitTags(tags string) []string {
+	// replace commas with spaces
+	tags = strings.Replace(tags, ",", " ", -1)
+	// split the tags
+	split := strings.Split(tags, " ")
+	return split
 }
